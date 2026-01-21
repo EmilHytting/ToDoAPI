df8adf16812e1580b031298503dc65efc281429d
Emil Hytting <nordstack@outlook.dk>
2026-01-21 11:54:55 +0100

    Add project files.

---
+++ Data/TodoDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Classes;
+
+namespace ToDoAPI.Data
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+        {
+		}
+
+        public DbSet<Todo> Todos => Set<Todo>();
+	}
+}
